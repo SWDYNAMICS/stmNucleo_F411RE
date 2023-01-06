56aeea8d68d292eeeb685059b59b9d38169e5f6e
LSW9609-PC00\USER <lsw9609@smartradarsystem.com>
2023-01-06 18:07:58 +0900

    some memo , api function coding

--- stmf411re_fw/src/hw/driver/flash.c
+++ stmf411re_fw/src/hw/driver/flash.c
@@ -11,8 +11,10 @@
 typedef struct
 {
   uint32_t addr;
-  uint32_t legnth;
+  uint32_t length;
 }flash_tbl_t;
+
+static bool flashInSector(uint16_t sector_num, uint32_t addr, uint32_t length);
 
 const flash_tbl_t flash_tbl[FLASH_SECTOR_MAX] =
 {
@@ -28,11 +30,19 @@ const flash_tbl_t flash_tbl[FLASH_SECTOR_MAX] =
 
 bool flashInit(void)
 {
-
+  return true;
 }
 bool flashErase(uint32_t addr, uint32_t length)
 {
+  bool ret = false;
+  HAL_StatusTypeDef status;
+  FLASH_EraseInitTypeDef init;
+  uint32_t sector_error;
 
+  status = HAL_FLASHEx_Erase(&init, &sector_error);
+  //YOUTUBE 31:58 END POINT
+
+  return ret;
 }
 bool flashWrite(uint32_t addr, uint8_t *p_data, uint32_t length)
 {
@@ -42,3 +52,42 @@ bool flashRead(uint32_t addr, uint8_t *p_data, uint32_t length)
 {
 
 }
+bool flashInSector(uint16_t sector_num, uint32_t addr, uint32_t length)
+{
+  bool ret = false;
+
+  uint32_t sector_start;
+  uint32_t sector_end;
+  uint32_t flash_start;
+  uint32_t flash_end;
+
+  sector_start = flash_tbl[sector_num].addr;
+  sector_end   = flash_tbl[sector_num].addr + flash_tbl[sector_num].length - 1;
+  flash_start  = addr;
+  flash_end    = addr + length - 1;
+
+  if(sector_start >= flash_start && sector_start <= flash_end)
+  {
+    ret = true;
+  }
+  if(sector_end >= flash_start && sector_end <= flash_end)
+  {
+    ret = true;
+  }
+  if(flash_start >= sector_start && flash_start <= sector_end)
+  {
+    ret = true;
+  }
+  if(flash_end >= sector_start && flash_end <= sector_end)
+  {
+    ret = true;
+  }
+  return ret;
+}
+
+
+
+
+
+
+
